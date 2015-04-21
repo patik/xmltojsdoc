242dfca182bd1dbd3bb8e977b50a81d9dc280508
Craig Patik <craig@patik.com>
2015-04-21 07:28:22 -0400

    Example file

---
+++ example.js
@@ -0,0 +1,35 @@
+// All three comment blocks in here should result in the same JSDoc output (except the number in the summary/description)
+
+var ignoreMe1;
+
+
+///<summary>Example 1 description</summary>
+///<param name="elem" type="Element">Button element</param>
+///<param name="options" type="Object">Optional settings</param>
+///<returns>True if successful, false if unsuccessful</returns>
+///<remark>If no `elem` is specified, the first `.button` on the page will be used</remark>
+
+var ignoreMe2;
+
+/**
+ * Adds event listeners to a particular tooltip
+ * @param   {Element}  tooltipElem  The div.tooltip
+ * @param   {Array}    links        Links within the tooltip (optional)
+ * @return  {[type]}                The target element
+ */
+
+var ignoreMe3;
+
+    ///<summary>Example 2 description</summary>
+    ///<param name="elem" type="Element">Button element</param>
+    ///<param name="options" type="Object">Optional settings</param>
+    ///<returns>True if successful, false if unsuccessful</returns>
+    ///<remark>If no `elem` is specified, the first `.button` on the page will be used</remark>
+
+///<summary>Example 3 description</summary>
+///<remark>If no `elem` is specified, the first `.button` on the page will be used</remark>
+///<param name="elem" type="Element">Button element</param>
+///<param name="options" type="Object">Optional settings</param>
+///<returns>True if successful, false if unsuccessful</returns>
+
+var ignoreMe4;
